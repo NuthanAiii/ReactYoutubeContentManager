54ebdeef3f7d262515d35129e8a5a8ffceab1b61
NuthanAiii <nuthanty2@gmail.com>
2026-01-29 08:30:16 +0530

    added cors config

--- Backend-YCM/fastapi-be-ycm/main.py
+++ Backend-YCM/fastapi-be-ycm/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from routers import content, authentication
+from fastapi.middleware.cors import CORSMiddleware
 
 import models
 from database import engine, SessionLocal
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:3000"],  # frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(content.router)
 app.include_router(authentication.router)
 
